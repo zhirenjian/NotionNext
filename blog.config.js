8ca157268f693508346f2f49bae46e3d789fad48
zhirenjian <110708136+zhirenjian@users.noreply.github.com>
2022-08-06 12:59:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: '执人间', // 作者
   BIO: '知我所能，我所能者尽善尽美，知我所不能，我所不能者虚怀若谷！', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://notion-next-sepia.vercel.app/', // 网站地址
   KEYWORDS: 'Notion, 博客,执方书院,执人间', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
