bf1a310a7d6a9be744fd676e294b3c290a1d81b6
执人间 <110708136+zhirenjian@users.noreply.github.com>
2022-08-26 17:59:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -43,9 +43,12 @@ const BLOG = {
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   
-  CONTACT_EMAIL: 'https://github.com/zhirenjian',
-  CONTACT_GITHUB: 'zhirenjian_0618tyy@163.com',
-
+ CONTACT_EMAIL: 'zhifangshuyuan.top',
+  CONTACT_WEIBO: '',
+  CONTACT_TWITTER: '',
+  CONTACT_GITHUB: 'https://github.com/zhirenjian',
+  CONTACT_TELEGRAM: '',
+  
   // 鼠标点击烟花特效
   FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 鼠标点击烟花特效
 
@@ -106,7 +109,7 @@ const BLOG = {
   COMMENT_VALINE_PLACEHOLDER: process.env.NEXT_PUBLIC_VALINE_PLACEHOLDER || '善语结善缘，恶语伤人心~', // 可以搭配后台管理评论 https://github.com/DesertsP/Valine-Admin  便于查看评论，以及邮件通知，垃圾评论过滤等功能
 
   COMMENT_WALINE_SERVER_URL: process.env.NEXT_PUBLIC_WALINE_SERVER_URL || '', // Waline 评论 @see https://waline.js.org/guide/get-started.html
-  COMMENT_WALINE_RECENT: process.env.NEXT_PUBLIC_WALINE_RECENT || true, // 最新评论
+  COMMENT_WALINE_RECENT: process.env.NEXT_PUBLIC_WALINE_RECENT || false, // 最新评论
 
   // 站点统计
   ANALYTICS_BUSUANZI_ENABLE: true, // 展示网站阅读量、访问数 see http://busuanzi.ibruce.info/
